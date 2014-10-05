063112d835e8d0feaa0b38ff47a14447c5723d88
Christian Le <christianle@berkeley.edu>
2014-10-04 21:06:18 -0700

    random generator

--- server.js
+++ server.js
@@ -4,11 +4,23 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 8080;
+// var port = process.env.PORT || 8888;
+var port = 8888;
 var sio = require('socket.io').listen(app.listen(port));
 
 // config ------------
 app.use(express.static(__dirname + '/public'));
+
+function generateRoom() {
+  var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var room = '';
+
+  for (var i = 0; i < 8; i += 1) {
+    room += chars.charAt(Math.floor(Math.random() * 62));
+  }
+
+  return room;
+}
 
 // routes ------------
 require('./app/routes')(app);

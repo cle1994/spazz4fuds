4e560c8c3eed247ab7ee85563ebe05441a11d256
Emily To <emilytoe@gmail.com>
2014-10-04 22:32:18 -0700

    backend update

--- app/routes.js
+++
@@ -1,8 +0,0 @@
-// app/routes.js
-
-module.exports = function(app) {
-  // Frontend routing to be handled by angular ==========
-  app.get('*', function(req, res) {
-    res.sendfile('./public/index.html');
-  });
-}
--- server.js
+++ server.js
@@ -11,19 +11,60 @@ var sio = require('socket.io').listen(app.listen(port));
 // config ------------
 app.use(express.static(__dirname + '/public'));
 
-function generateRoom() {
-  var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  var room = '';
+app.get('*', function(req, res) {
+  res.sendFile('./public/index.html');
+});
 
-  for (var i = 0; i < 8; i += 1) {
-    room += chars.charAt(Math.floor(Math.random() * 62));
-  }
+var fudz = {};
+var keys = {
+    right: 0,
+    left: 1,
+    up: 2,
+    down: 3
+};
 
-  return room;
-}
+var count = {u: 0, d: 0, l: 0, r: 0};
 
-// routes ------------
-require('./app/routes')(app);
+sio.sockets.on('connection', function(socket) {
+    socket.on('join', function(data) {
+        socket.join(data.room);
+        socket.set('room', data.room);
+        socket.emit('position', fudz[data.room][pos]);
+    });
+
+    socket.on('move', function(key) {
+        if (key == up) {
+            count.u++
+        } else if (key == down) {
+            count.d++;
+        } else if (key == right) {
+            count.r++;
+        } else if (key == left) {
+            count.l++;
+        }
+        val max = 0;
+        val node = '';
+
+        for (dir in count) {
+            var temp = max;
+            max = Math.max(count.dir.value, max);
+            if (temp != max) {
+                node = dir;
+            }
+        }
+        socket.emit('move', node);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('Disconnected');
+        socket.get('room', function(err, room) {
+            sio.sockets.in(room).emit('leave');
+            if (room in fudz) {
+                del fudz.room;
+            }
+        });
+    });
+});
 
 console.log('Magic happens on port ' + port);
 exports = module.exports = app;
